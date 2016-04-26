8f39c6e76a4f1ef54013c492bc87649c2a0d022e
David Guan <DavidGuanDev@gmail.com>
2016-04-26 15:21:02 +0800

    Crawling WB indicator pages.

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
+"use strict"
 const Crawler = require('crawler')
 
+let counter = 0
 const connection = new Crawler({
-  maxConnections: 10
+  //Set bigger number here if your network works well
+  maxConnections: 2,
+  callback: (err, res, $) => {
+    const content = $('.tab-item').html()
+    if (content && content.indexOf('Chart') !== -1) {
+      console.log('Chart supported')
+    } else {
+      console.log(`Chart not supported, Count ${++counter}`)
+    }
+  }
 })
 
+const urlArray = []
 connection.queue({
-  uri: 'https://www.google.com/',
+  url: 'http://data.wb.dev.wiredcraft.com/indicator',
   callback: (err, res, $) => {
-    console.log('**************')
-    console.log('**************')
-    console.log('google fetched')
-    console.log('**************')
-    console.log('**************')
-    console.log($('body').html())
+    $('section.nav-item ul a').each(function(i, elem) {
+      //url pattern: /indicator/SL.EMP.VULN.ZS?view=chart
+      const pattern = /^\/indicator/
+      const href = $(this).attr('href')
+      if (href && pattern.test(href)) {
+        console.log(`http://data.wb.dev.wiredcraft.com${href}`)
+        urlArray.push({ url: `http://data.wb.dev.wiredcraft.com${href}` })
+      }
+    })
+    console.log('urls fetched')
+    connection.queue(urlArray)
   }
 })
